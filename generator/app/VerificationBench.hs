05c8bfa994bd3f5498795e3a0a70424b69261e79
Andreas Triantafyllos <andreas.triantafyllos@iohk.io>
2018-09-03 16:13:05 +0300

    [CBR-275] Added compatible instances

--- generator/app/VerificationBench.hs
+++ generator/app/VerificationBench.hs
@@ -39,10 +39,10 @@ import           Pos.Launcher.Configuration (ConfigurationOptions (..),
                      HasConfigurations, defaultConfigurationOptions,
                      withConfigurationsM)
 import           Pos.Util.CompileInfo (withCompileInfo)
+import           Pos.Util.Log.LoggerConfig (defaultInteractiveConfiguration)
 import           Pos.Util.Util (realTime)
-import           Pos.Util.Wlog (LoggerConfig, LoggerName (..), consoleActionB,
-                     debugPlus, defaultHandleAction, logError, logInfo,
-                     setupLogging, termSeveritiesOutB)
+import           Pos.Util.Wlog (LoggerConfig, Severity (Debug), logError,
+                     logInfo, setupLogging)
 
 import           Test.Pos.Block.Logic.Mode (BlockTestMode, TestParams (..),
                      runBlockTestMode)
@@ -182,7 +182,7 @@ readBlocks path = do
 
 main :: IO ()
 main = do
-    setupLogging Nothing loggerConfig
+    setupLogging loggerConfig
     args <- Opts.execParser
         $ Opts.info
             (benchArgsParser <**> Opts.helper)
@@ -197,7 +197,7 @@ main = do
             , cfoSystemStart = Just (Timestamp startTime)
             }
     withCompileInfo $
-        withConfigurationsM (LoggerName "verification-bench") Nothing Nothing False cfo $ \ !coreConfig !_ !txpConfig !_ -> do
+        withConfigurationsM "verification-bench" Nothing Nothing False cfo $ \ !coreConfig !_ !txpConfig !_ -> do
             let coreConfig' = coreConfig
                     { configProtocolConstants =
                         (configProtocolConstants coreConfig) { pcK = baK args }
@@ -260,8 +260,7 @@ main = do
                         traverse_ (logError . show) errs
     where
         loggerConfig :: LoggerConfig
-        loggerConfig = termSeveritiesOutB debugPlus
-                <> consoleActionB defaultHandleAction
+        loggerConfig = defaultInteractiveConfiguration Debug
 
         avarage :: [Float] -> Float
         avarage as = sum as / realToFrac (length as)
